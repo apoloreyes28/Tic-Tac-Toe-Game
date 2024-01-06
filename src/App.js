bbbbde2bf9a344a936acd08e44feccfef8c89ce5
apoloreyes28 <reyesapolinar31@gmail.com>
2024-01-06 10:05:27 -0700

    Making an interactive component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,59 @@
-function Square({ value }) {
-  // indica que al componente Square 
-  // se le puede pasar un prop llamado value
+import { useState } from 'react';
 
-  return <button className="square">{ value }</button>;
+// cambie { value } = prop, por useState() = hook, ya que un prop nos permite 
+// pasar información estatica, mientras que un hook nos permite recordar cosas.
+function Square() {
+
+  const [value, setValue] = useState(null);
+  // value almacena el valor y setValue es una función que se puede utilizar para 
+  // cambiar el valor. null es el valor inicial para esta variable de estado: value
+
+  function handleClick() {
+ // console.log('clicked!');
+    setValue('X');
+  }
+  // declaramos una función que se va ejecutar cada vez que 
+  // demos click en el botón (escuche el evento)
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
   return (
     <>
       <div className="board-row">
-
-        {/* modifico esta etiqueta: <button className="square">1</button>
-            en un nuevo componente 
-            mucho mas corto y mas 
-            facil de manipular.  */}
-
-        <Square value="1"  />
-        <Square value="2"  />
-        <Square value="3"  />
-
-        {/* <Square/> paso a ser <Square value="1"/> 
-             ya que aqui le pasamos un prop: { value } */}
-
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4"  />
-        <Square value="5"  />
-        <Square value="6"  />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7"  />
-        <Square value="8"  />
-        <Square value="9"  />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
 }
+
+/*
+Al llamar a esta función set() desde un controlador (evento) onClick(), 
+le estás diciendo a React que vuelva a renderizarla.
+
+
+Cada Square() tiene su propio estado: lo almacenado en value para cada Square() 
+es completamente independiente de los demás. Cuando llamas a una función set() 
+en un componente, React actualiza automáticamente los componentes secundarios
+internos también.
+*/
