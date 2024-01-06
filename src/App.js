2649a31b962845b38bd2beb0a6f030b3f0e4d79b
apoloreyes28 <reyesapolinar31@gmail.com>
2024-01-06 10:01:06 -0700

    Building the board

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 /*
-El código App.js crea un componente. 
-
-En React, un componente es una pieza de código reutilizable que representa 
-una parte de una interfaz de usuario. 
-
-Los componentes se utilizan para representar, administrar y actualizar los 
-elementos de la interfaz de usuario en su aplicación.
+Los componentes de React deben devolver un único elemento JSX y no varios 
+elementos JSX adyacentes.
 */
 
-export default function Square() {
-  return <button className="square">X</button>;
+// Square = cuadrado -> Board() = tablero
+export default function Board() {
+  return (
+      // envolver varios elementos JSX adyacentes
+    <>
+      <div className="board-row">
+                  {/* fila */}
+
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+                       {/* cuadrado = 1  2  3, inline-elements */}
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </>
+  );
 }
-
-/*
-export, permite que sea accesible fuera de este archivo.
-
-default, le dice a otros archivos que usan este código que es la función 
-principal de este archivo.
-*/
