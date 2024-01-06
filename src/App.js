83e79c0b234ac16914f55c6fa60cd6ce9a758e01
apoloreyes28 <reyesapolinar31@gmail.com>
2024-01-06 10:09:50 -0700

    Taking turns

--- src/App.js
+++ src/App.js
@@ -1,68 +1,43 @@
 import { useState } from 'react';
 
-/*
-Cada componente Square mantiene una parte del estado del juego. 
-Para comprobar si hay un ganador en un juego de tres en raya, sería necesario 
-que Board conozca de alguna manera el estado de cada uno de los 9 componentes Square.
-
-El mejor enfoque es almacenar el estado del juego en el componente principal Board 
-en lugar de cada Square. El componente Board puede decirle a cada Square qué mostrar 
-pasando un prop, como lo hizo cuando pasó un número a cada cuadrado.
-
-Elevar el estado a un componente principal es común cuando se refactorizan los 
-componentes de React.
-*/
 function Square({ value, onSquareClick }) {
   return (
-    // creamos una forma de actualizar el estado
     <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
-  /*
-  El componente Square() va recibir el prop: value, del componente Board(). 
-  
-  Esto requerirá eliminar el seguimiento de estado del componente Square(), 
-  el value y el onClick del botón.
-
-  Cada cuadrado ahora recibirá un valueaccesorio que será 'X', 'O' o null
-  para cuadrados vacíos.
-  */
 }
 
 export default function Board() {
 
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
-  /*
-  Declaramos una variable de estado: squares, cuyo nombre predeterminado 
-  sea una matriz de 9 valores nulos correspondientes a los 9 cuadrados
-
-  Array(9).fill(null) crea una matriz con nueve elementos y establece cada 
-  uno de ellos en null.
-
-  Cada entrada de la matriz corresponde al valor de un cuadrado. 
-  Cuando completes el tablero más tarde, la matriz squares se verá así:
-
-  ['O', null, 'X', 'X', 'X', 'O', 'O', null, null]
-  */
 
   function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
+    /*
+    Si el cuadrado ya está lleno, entrará return en la función handleClick(), 
+    antes de que intente actualizar el estado del tablero.
+    */
+
     const nextSquares = squares.slice();
-
-    nextSquares[i] = "X";
-    // índice del cuadrado a actualizar
-
+    /*
+    Cada vez que un jugador se mueve, xIsNextse invertirá (un booleano) para 
+    determinar qué jugador sigue y se guardará el estado del juego. 
+    
+    Actualizará la función Board' handleClickpara invertir el valor de xIsNext
+    */
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+ // nextSquares[i] = "X";
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
-  /*
-  La función handleClick() crea una copia de la matriz squares (nextSquares) 
-  con el método de JS: slice()
-
-  Llamar a la función setSquares le permite a React saber que el estado del 
-  componente ha cambiado. Esto activará una nueva representación de los 
-  componentes que usan el estado squares(Board), así como de sus componentes 
-  secundarios (los componentes Square que componen el tablero).
-  */
 
   return (
     <>
@@ -70,13 +45,6 @@ export default function Board() {
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-
-         {/* 
-         Aquí () => handleClick(0) hay una función de flecha, que es una forma 
-         más corta de definir funciones. => Cuando se hace clic en el cuadrado, 
-         se ejecutará el código después de la "flecha", llamando a handleClick(0).
-         */}
-
       </div>
       <div className="board-row">
         <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
